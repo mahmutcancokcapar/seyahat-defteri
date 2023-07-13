00d8179acf37f7f051bd163dec12310ed7a17d62
Mahmut Can Çokçapar <103719457+mahmutcancokcapar@users.noreply.github.com>
2023-07-13 10:44:28 +0300

    Update place_card.dart
    birkaç değişiklik

--- lib/sayfalar/place_card.dart
+++ lib/sayfalar/place_card.dart
@@ -1,18 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-
-import '../service/auth.dart';
-
-final User? user = Auth().currentUser;
-
-Widget _userUid() {
-  return Text(
-    user?.email ?? 'User email',
-    style: GoogleFonts.indieFlower(),
-  );
-}
 
 Widget placeCard(Function()? onTap, QueryDocumentSnapshot doc) {
   return InkWell(
@@ -26,114 +14,145 @@ Widget placeCard(Function()? onTap, QueryDocumentSnapshot doc) {
         color: const Color.fromARGB(146, 236, 196, 241),
         borderRadius: BorderRadius.circular(8),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          /*Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 300,
-                height: 180,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  border: Border.all(),
-                ),
-                child: const Center(
-                  child: Text('Buraya resim gelecek'),
-                ),
-              ),
-            ],
-          ),*/
-          const SizedBox(
-            height: 10,
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
+      child: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            /*Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  doc['title'],
-                  style: GoogleFonts.spaceGrotesk(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
+                Container(
+                  width: 300,
+                  height: 180,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(),
                   ),
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(
-              left: 50,
-              right: 50,
-              bottom: 5,
-            ),
-            child: Divider(),
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 20,
-                  child: Text(
-                    doc['description'],
-                    style: GoogleFonts.indieFlower(
-                      color: Colors.grey,
-                    ),
-                    overflow: TextOverflow.ellipsis,
+                  child: const Center(
+                    child: Text('Buraya resim gelecek'),
                   ),
                 ),
               ],
+            ),*/
+            const SizedBox(
+              height: 10,
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 50, right: 50, top:5, bottom: 10,),
-            child: Divider(),
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 25,
-                  child: Text(
-                    doc['date'],
+            SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    doc['title'],
                     style: GoogleFonts.spaceGrotesk(
-                      color: Colors.grey,
-                      fontSize: 12,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
                     ),
                     overflow: TextOverflow.ellipsis,
                   ),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(
+                left: 50,
+                right: 50,
+                bottom: 5,
+              ),
+              child: Divider(),
+            ),
+            SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 20,
+                    child: Text(
+                      doc['description'],
+                      style: GoogleFonts.indieFlower(
+                        color: Colors.grey,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 50, right: 50, top:5, bottom: 10,),
+              child: Divider(),
+            ),
+            SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 25,
+                    child: Text(
+                      doc['date'],
+                      style: GoogleFonts.spaceGrotesk(
+                        color: Colors.grey,
+                        fontSize: 12,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 50, right: 50, top:5, bottom: 10,),
+              child: Divider(),
+            ),
+            SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 25,
+                    child: Text(
+                      doc['userEmail'],
+                      style: GoogleFonts.spaceGrotesk(
+                        color: Colors.grey,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            /*Row(
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(147, 179, 117, 186),
+                  ),
+                  onPressed: () {},
+                  child: const Icon(Icons.favorite_border_outlined),
+                ),
+                const Spacer(),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(147, 179, 117, 186),
+                  ),
+                  onPressed: () {},
+                  child: const Icon(Icons.arrow_outward_rounded),
                 ),
               ],
-            ),
-          ),
-          /*Row(
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(147, 179, 117, 186),
-                ),
-                onPressed: () {},
-                child: const Icon(Icons.favorite_border_outlined),
-              ),
-              const Spacer(),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(147, 179, 117, 186),
-                ),
-                onPressed: () {},
-                child: const Icon(Icons.arrow_outward_rounded),
-              ),
-            ],
-          ),*/
-        ],
+            ),*/
+          ],
+        ),
       ),
     ),
   );
