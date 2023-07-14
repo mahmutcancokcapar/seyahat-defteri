5f0754421276a9e7fd98e34828df55a4e66d8cf3
Mahmut Can Çokçapar <103719457+mahmutcancokcapar@users.noreply.github.com>
2023-07-14 08:25:34 +0300

    Update register.dart

--- lib/sayfalar/register.dart
+++ lib/sayfalar/register.dart
@@ -1,9 +1,9 @@
+import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:seydef/sayfalar/email_verification_page.dart';
 import 'package:seydef/sayfalar/login_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
-import '../service/auth.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -12,22 +12,54 @@ class RegisterPage extends StatefulWidget {
   State<RegisterPage> createState() => _RegisterPageState();
 }
 
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
 class _RegisterPageState extends State<RegisterPage> {
   TextEditingController remailController = TextEditingController();
   TextEditingController rpasswordController = TextEditingController();
-
+  bool _isLoading = false;
   String? errorMessage = '';
 
-  Future<void> createUser() async {
+  Future<void> registerUser(BuildContext context) async {
     try {
-      await Auth().createUser(
-        email: remailController.text,
-        password: rpasswordController.text,
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: remailController.text.trim(),
+        password: rpasswordController.text.trim(),
       );
-    } on FirebaseAuthException catch (e) {
-      setState(() {
-        errorMessage = e.message;
-      });
+      await userCredential.user!.sendEmailVerification();
+      // Kayıt başarılı, doğrulama epostası gönderildi
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => EmailVerification(),
+        ),
+      );
+    } catch (e) {
+      // Hata oldu, kayıt başarısız
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text(
+                'Kayıt Başarısız',
+                style: GoogleFonts.spaceGrotesk(),
+              ),
+              content: Text(
+                'Bir hata oluştu, lütfen tekrar deneyiniz.',
+                style: GoogleFonts.indieFlower(),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Text(
+                    'Tamam',
+                    style: GoogleFonts.spaceGrotesk(),
+                  ),
+                ),
+              ],
+            );
+          });
     }
   }
 
@@ -60,7 +92,7 @@ class _RegisterPageState extends State<RegisterPage> {
               textAlign: TextAlign.start,
             ),
             const SizedBox(
-              height: 40,
+              height: 95,
             ),
             TextField(
               controller: remailController,
@@ -94,15 +126,37 @@ class _RegisterPageState extends State<RegisterPage> {
             const SizedBox(
               height: 10,
             ),
-            ElevatedButton(
-              onPressed: () {
-                createUser();
-              },
-              child: Text(
-                'Kayıt Ol',
-                style: GoogleFonts.spaceGrotesk(fontSize: 20),
-              ),
-            ),
+            _isLoading
+                ? const CircularProgressIndicator()
+                : ElevatedButton(
+                    onPressed: () {
+                      registerUser(context);
+                      //createUser();
+                      /*
+                      setState(() {
+                        _isLoading = true;
+                      });
+                      await signUp(
+                          userEmail: remailController.text,
+                          userPassword: rpasswordController.text,
+                          context: context);
+                      if (remailController.text.isNotEmpty) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (ctx) => const EmailVerification(),
+                          ),
+                        );
+                      }
+                      setState(() {
+                        _isLoading = false;
+                      });*/
+                    },
+                    child: Text(
+                      'Kayıt Ol',
+                      style: GoogleFonts.spaceGrotesk(fontSize: 20),
+                    ),
+                  ),
             const SizedBox(
               width: 10,
             ),
