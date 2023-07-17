24a6031335cc261426ccc06701455942f58bd0f5
Mahmut Can Çokçapar <103719457+mahmutcancokcapar@users.noreply.github.com>
2023-07-17 10:46:28 +0300

    Update settings.dart

--- lib/sayfalar/setting_/settings.dart
+++ lib/sayfalar/setting_/settings.dart
@@ -6,26 +6,11 @@ import 'package:seydef/sayfalar/setting_/gonderi_silme.dart';
 import 'package:seydef/sayfalar/setting_/hakkimizda.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-final Uri instagram = Uri.parse('https://www.instagram.com');
-final Uri facebook = Uri.parse('https://tr-tr.facebook.com');
-final Uri twitter = Uri.parse(
-    'https://twitter.com/i/flow/login?redirect_after_login=%2F%3Flang%3Dtr');
+final Uri instagram = Uri.parse('https://www.instagram.com/mc_medyaa/');
 
 Future<void> _launchURLinsta() async {
   if (!await launchUrl(instagram)) {
     throw 'Could not launch $instagram';
-  }
-}
-
-Future<void> _launchURLfacebook() async {
-  if (!await launchUrl(facebook)) {
-    throw 'Could not launch $facebook';
-  }
-}
-
-Future<void> _launchURLtwitter() async {
-  if (!await launchUrl(twitter)) {
-    throw 'Could not launch $twitter';
   }
 }
 
@@ -169,7 +154,7 @@ class _SettingsState extends State<Settings> {
       showDragHandle: true,
       context: context,
       builder: (context) => SizedBox(
-        height: 500,
+        height: 150,
         child: Column(
           children: [
             InkWell(
@@ -188,54 +173,6 @@ class _SettingsState extends State<Settings> {
                   padding: const EdgeInsets.all(10.0),
                   child: Text(
                     'Instagram',
-                    style: GoogleFonts.spaceGrotesk(
-                      fontSize: 25,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-            InkWell(
-              onTap: () {
-                _launchURLfacebook();
-              },
-              child: Container(
-                width: double.infinity,
-                height: 60,
-                margin: const EdgeInsets.all(32),
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Text(
-                    'Facebook',
-                    style: GoogleFonts.spaceGrotesk(
-                      fontSize: 25,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-            InkWell(
-              onTap: () {
-                _launchURLtwitter();
-              },
-              child: Container(
-                width: double.infinity,
-                height: 60,
-                margin: const EdgeInsets.all(32),
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Text(
-                    'Twitter',
                     style: GoogleFonts.spaceGrotesk(
                       fontSize: 25,
                     ),
