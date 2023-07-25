8206dcd83495e3a4f892806b205469f665dedf91
Mahmut Can Çokçapar <103719457+mahmutcancokcapar@users.noreply.github.com>
2023-07-25 14:24:22 +0300

    Update login_page.dart

--- lib/sayfalar/login_page.dart
+++ lib/sayfalar/login_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:seydef/sayfalar/discover_page.dart';
 import 'package:seydef/sayfalar/register.dart';
 import '../service/auth.dart';
 
@@ -16,182 +17,215 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
-  String? errorMessage = '';
-
+  String errorMessage = '';
+  bool isButtonEnabled = false;
   bool isTurkish = true;
+
+  void checkField() {
+    setState(() {
+      isButtonEnabled =
+          emailController.text.isNotEmpty && passwordController.text.isNotEmpty;
+    });
+  }
 
   Future<void> signIn() async {
     try {
-      await Auth().signIn(
-        email: emailController.text.trim(),
-        password: passwordController.text.trim(),
-      );
-    } on FirebaseAuthException catch (e) {
-      setState(() {
-        errorMessage = e.message;
-      });
+      await Auth()
+          .signIn(
+            email: emailController.text.trim(),
+            password: passwordController.text.trim(),
+          )
+          .then(
+            (value) => Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Discover(),
+              ),
+            ),
+          );
+    } catch (e) {
+      if (e is FirebaseAuthException) {
+        if (e.code == 'user-not-found') {
+          errorMessage = 'kullaniciBulunamadi'.tr;
+        } else if (e.code == 'wrong-password') {
+          errorMessage = 'hataliSifre'.tr;
+        } else if (e.code == 'invalid-email') {
+          errorMessage = 'gecersizEposta'.tr;
+        } else {
+          errorMessage = 'hata'.tr;
+        }
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(errorMessage),
+            duration: Duration(seconds: 3),
+          ),
+        );
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 50,
-            ),
-            Text(
-              'baslik'.tr,
-              style: GoogleFonts.indieFlower(
-                fontSize: 40,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              'hg'.tr,
-              style: GoogleFonts.spaceGrotesk(
-                fontSize: 50,
-              ),
-              textAlign: TextAlign.start,
-            ),
-            const SizedBox(
-              height: 95,
-            ),
-            TextField(
-              controller: emailController,
-              keyboardType: TextInputType.emailAddress,
-              style: GoogleFonts.indieFlower(),
-              decoration: InputDecoration(
-                labelText: 'emailAdres'.tr,
-                focusColor: Colors.black,
-                labelStyle: GoogleFonts.indieFlower(
-                  color: const Color.fromARGB(255, 130, 126, 126),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              style: GoogleFonts.indieFlower(),
-              keyboardType: TextInputType.number,
-              maxLength: 6,
-              decoration: InputDecoration(
-                labelText: 'sifre'.tr,
-                focusColor: Colors.black,
-                labelStyle: GoogleFonts.indieFlower(
-                  color: const Color.fromARGB(255, 130, 126, 126),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(147, 179, 117, 186),
-              ),
-              onPressed: () {
-                signIn();
-              },
-              child: Text(
-                'giris'.tr,
-                style: GoogleFonts.spaceGrotesk(fontSize: 20),
-              ),
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            Row(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
               children: [
-                const Spacer(),
-                Text(
-                  'hesapYok'.tr,
-                  style: GoogleFonts.indieFlower(fontSize: 15),
+                const SizedBox(
+                  height: 50,
                 ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const RegisterPage(),
-                      ),
-                    );
-                  },
-                  child: Text(
-                    'kayitOl'.tr,
-                    style: GoogleFonts.indieFlower(
-                        color: Colors.blue, fontSize: 15),
+                Text(
+                  'baslik'.tr,
+                  style: GoogleFonts.indieFlower(
+                    fontSize: 40,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'hg'.tr,
+                  style: GoogleFonts.spaceGrotesk(
+                    fontSize: 45,
+                  ),
+                  textAlign: TextAlign.start,
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                TextField(
+                  controller: emailController,
+                  onChanged: (value) => checkField(),
+                  keyboardType: TextInputType.emailAddress,
+                  style: GoogleFonts.indieFlower(),
+                  decoration: InputDecoration(
+                    labelText: 'emailAdres'.tr,
+                    focusColor: Colors.black,
+                    labelStyle: GoogleFonts.indieFlower(
+                      color: const Color.fromARGB(255, 130, 126, 126),
+                    ),
                   ),
                 ),
-                const Spacer(),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  onChanged: (value) => checkField(),
+                  controller: passwordController,
+                  obscureText: true,
+                  style: GoogleFonts.indieFlower(),
+                  keyboardType: TextInputType.number,
+                  maxLength: 6,
+                  decoration: InputDecoration(
+                    labelText: 'sifre'.tr,
+                    focusColor: Colors.black,
+                    labelStyle: GoogleFonts.indieFlower(
+                      color: const Color.fromARGB(255, 130, 126, 126),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(147, 179, 117, 186),
+                  ),
+                  onPressed: isButtonEnabled ? signIn : null,
+                  child: Text(
+                    'giris'.tr,
+                    style: GoogleFonts.spaceGrotesk(fontSize: 20),
+                  ),
+                ),
+                Row(
+                  children: [
+                    const Spacer(),
+                    Text(
+                      'hesapYok'.tr,
+                      style: GoogleFonts.indieFlower(fontSize: 15),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const RegisterPage(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        'kayitOl'.tr,
+                        style: GoogleFonts.indieFlower(
+                            color: Colors.blue, fontSize: 15),
+                      ),
+                    ),
+                    const Spacer(),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Get.updateLocale(
+                            const Locale("tr", "TR"),
+                          );
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Text(
+                            'Türkçe',
+                            style: GoogleFonts.spaceGrotesk(
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 30,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Get.updateLocale(
+                            const Locale("en", "EN"),
+                          );
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Text(
+                            'English',
+                            style: GoogleFonts.spaceGrotesk(
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      Get.updateLocale(
-                        const Locale("tr", "TR"),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(12),
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Text(
-                        'Türkçe',
-                        style: GoogleFonts.spaceGrotesk(
-                          fontSize: 15,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 30,
-                  ),
-                  InkWell(
-                    onTap: () {
-                      Get.updateLocale(
-                        const Locale("en", "EN"),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(12),
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Text(
-                        'English',
-                        style: GoogleFonts.spaceGrotesk(
-                          fontSize: 15,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const Spacer(),
-          ],
+          ),
         ),
       ),
     );
