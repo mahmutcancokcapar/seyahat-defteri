e36fe01c8b0ae193b9c8beabcce3e43cd6db5354
Mahmut Can Çokçapar <103719457+mahmutcancokcapar@users.noreply.github.com>
2023-07-28 08:27:16 +0300

    Update place_card.dart

--- lib/sayfalar/place_card.dart
+++ lib/sayfalar/place_card.dart
@@ -1,169 +1,219 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-Widget placeCard(Function()? onTap, QueryDocumentSnapshot doc) {
-  return InkWell(
-    onTap: onTap,
-    child: Container(
-      width: 10,
-      height: 20,
-      padding: const EdgeInsets.all(8),
-      margin: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(146, 236, 196, 241),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            /*Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 300,
-                  height: 180,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all(),
-                  ),
-                  child: const Center(
-                    child: Text('Buraya resim gelecek'),
-                  ),
-                ),
-              ],
-            ),*/
-            const SizedBox(
-              height: 10,
-            ),
-            SingleChildScrollView(
-              physics: const BouncingScrollPhysics(),
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    doc['title'],
-                    style: GoogleFonts.spaceGrotesk(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
+class PlaceCard extends StatefulWidget {
+  final Function()? onTap;
+  final QueryDocumentSnapshot doc;
+  final String userUID; // User UID'yi almak için parametre olarak ekledik
+
+  PlaceCard({required this.onTap, required this.doc, required this.userUID});
+
+  @override
+  State<PlaceCard> createState() => _PlaceCardState();
+}
+
+class _PlaceCardState extends State<PlaceCard> {
+  void _openInGoogleMaps() async {
+    String googleMapsUrl =
+        'https://www.google.com/maps/search/?api=1&query=${widget.doc['kayitliKonum']}';
+    if (await canLaunch(googleMapsUrl)) {
+      await launch(googleMapsUrl);
+    } else {
+      throw 'Google Haritalar açılamadı.';
+    }
+  }
+
+  bool isFavorite = false;
+
+  void _toggleFavorite() async {
+    final docRef =
+        FirebaseFirestore.instance.collection('places').doc(widget.doc.id);
+
+    // Firestore'dan alınan belgedeki veriyi bir map olarak alıyoruz
+    Map<String, dynamic>? data = widget.doc.data() as Map<String, dynamic>?;
+
+    // Veriyi kontrol ediyoruz ve "favoriteBy" alanı yoksa oluşturuyoruz
+    if (data != null && !data.containsKey('favoriteBy')) {
+      await docRef.set({'favoriteBy': []}, SetOptions(merge: true));
+    }
+
+    // "favoriteBy" alanını güncellemek için belgedeki veriyi tekrar alıyoruz
+    data = widget.doc.data() as Map<String, dynamic>?;
+
+    // "favoriteBy" alanını alınan belgedeki veri türüne uygun bir liste olarak alıyoruz
+    List<dynamic>? favorites = data?['favoriteBy'];
+
+    // "favorites" listesini String tipinde bir liste haline getiriyoruz
+    List<String> stringFavorites = favorites?.cast<String>() ?? [];
+
+    if (stringFavorites.contains(widget.userUID)) {
+      // Eğer kullanıcının UID'si zaten favorilerde ise, favorilerden çıkar
+      stringFavorites.remove(widget.userUID);
+    } else {
+      // Değilse, favorilere ekle
+      stringFavorites.add(widget.userUID);
+    }
+
+    // "favoriteBy" alanını güncelle
+    await docRef.update({'favoriteBy': stringFavorites});
+
+    setState(() {
+      isFavorite = stringFavorites.contains(widget.userUID);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // "favoriteBy" alanındaki kullanıcının UID'sinin favorilerde olup olmadığını kontrol et
+    Map<String, dynamic>? data = widget.doc.data() as Map<String, dynamic>?;
+
+    if (data != null && data.containsKey('favoriteBy')) {
+      List<dynamic>? favorites = data['favoriteBy'];
+      List<String> stringFavorites = favorites?.cast<String>() ?? [];
+      isFavorite = stringFavorites.contains(widget.userUID);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: widget.onTap,
+      child: Container(
+        width: 10,
+        height: 20,
+        padding: const EdgeInsets.all(8),
+        margin: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(146, 236, 196, 241),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 10,
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(
-                left: 50,
-                right: 50,
-                bottom: 5,
-              ),
-              child: Divider(),
-            ),
-            SingleChildScrollView(
-              physics: const BouncingScrollPhysics(),
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 20,
-                    child: Text(
-                      doc['description'],
-                      style: GoogleFonts.indieFlower(
-                        color: Colors.grey,
-                      ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(
-                left: 50,
-                right: 50,
-                top: 5,
-                bottom: 10,
-              ),
-              child: Divider(),
-            ),
-            SingleChildScrollView(
-              physics: const BouncingScrollPhysics(),
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 25,
-                    child: Text(
-                      doc['date'],
+              SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      widget.doc['title'],
                       style: GoogleFonts.spaceGrotesk(
-                        color: Colors.grey,
-                        fontSize: 12,
-                      ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(
-                left: 50,
-                right: 50,
-                top: 5,
-                bottom: 10,
-              ),
-              child: Divider(),
-            ),
-            SingleChildScrollView(
-              physics: const BouncingScrollPhysics(),
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 25,
-                    child: Text(
-                      doc['userEmail'],
-                      style: GoogleFonts.spaceGrotesk(
-                        color: Colors.grey,
-                        fontSize: 12,
+                        color: Colors.black,
                         fontWeight: FontWeight.bold,
                       ),
                       overflow: TextOverflow.ellipsis,
                     ),
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(
+                  left: 50,
+                  right: 50,
+                  bottom: 5,
+                ),
+                child: Divider(),
+              ),
+              SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 20,
+                      child: Text(
+                        widget.doc['description'],
+                        style: GoogleFonts.indieFlower(
+                          color: Colors.grey,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(
+                  left: 50,
+                  right: 50,
+                ),
+                child: Divider(),
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: _toggleFavorite,
+                    icon: Icon(
+                      isFavorite ? Icons.favorite : Icons.favorite_border,
+                      color: isFavorite ? Colors.red : Colors.grey,
+                    ),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    onPressed: _openInGoogleMaps,
+                    icon: Icon(Icons.arrow_outward_outlined),
                   ),
                 ],
               ),
-            ),
-            /*Row(
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(147, 179, 117, 186),
-                  ),
-                  onPressed: () {},
-                  child: const Icon(Icons.favorite_border_outlined),
+              const SizedBox(
+                height: 5,
+              ),
+              SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 25,
+                      child: Text(
+                        widget.doc['userEmail'],
+                        style: GoogleFonts.spaceGrotesk(
+                          color: Colors.grey,
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
                 ),
-                const Spacer(),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(147, 179, 117, 186),
-                  ),
-                  onPressed: () {},
-                  child: const Icon(Icons.arrow_outward_rounded),
+              ),
+              SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 25,
+                      child: Text(
+                        widget.doc['date'],
+                        style: GoogleFonts.spaceGrotesk(
+                          color: Colors.grey,
+                          fontSize: 12,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),*/
-          ],
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
