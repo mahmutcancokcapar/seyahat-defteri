2c40851c4dfadd18ac5cd9505670b7047b427928
Mahmut Can Çokçapar <103719457+mahmutcancokcapar@users.noreply.github.com>
2023-07-28 08:23:42 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,38 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
 import 'package:seydef/sayfalar/dil.dart';
 import 'package:seydef/service/widget_tree.dart';
+
+class LocationProvider with ChangeNotifier {
+  LatLng? _selectedLocation;
+  bool isLocationSelected = false;
+
+  LatLng? get selectedLocation => _selectedLocation;
+
+  void updateLocation(LatLng location) {
+    _selectedLocation = location;
+    isLocationSelected = true;
+    notifyListeners();
+  }
+}
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => LocationProvider(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
@@ -21,7 +40,7 @@ class MyApp extends StatelessWidget {
       locale: Get.locale ?? Get.deviceLocale,
       fallbackLocale: Dil.varsayilan,
       debugShowCheckedModeBanner: false,
-      title: 'Seyahat Defterim',
+      title: 'Seyahat Defteri',
       home: const WidgetTree(),
     );
   }
