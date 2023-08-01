742efd5a5f74b59a0af6c03be7d734207f94842c
Mahmut Can Çokçapar <103719457+mahmutcancokcapar@users.noreply.github.com>
2023-08-01 10:43:26 +0300

    Update search_page.dart

--- lib/sayfalar/search_page.dart
+++ lib/sayfalar/search_page.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -17,6 +18,9 @@ class _SearchPageState extends State<SearchPage> {
   List<DocumentSnapshot> _searchResults = [];
   String userUID = ''; // Kullanıcının UID'sini burada tanımlayın
 
+  BannerAd? _bannerAd;
+  bool _isBannerAdLoaded = false;
+
   Future<void> _search() async {
     if (_searchQuery.trim().isEmpty) return;
 
@@ -28,6 +32,35 @@ class _SearchPageState extends State<SearchPage> {
     setState(() {
       _searchResults = snapshot.docs;
     });
+  }
+
+  void _loadBannerAd() {
+    _bannerAd = BannerAd(
+      size: AdSize.fullBanner,
+      adUnitId:
+          "ca-app-pub-7677750212299055/5128487105", // Reklam birimi kimliği
+      listener: BannerAdListener(
+        onAdLoaded: (_) {
+          setState(() {
+            _isBannerAdLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (ad, error) {
+          // ignore: avoid_print
+          print('BannerAd failed to load: $error');
+          ad.dispose();
+        },
+      ),
+      request: const AdRequest(),
+    );
+
+    _bannerAd!.load();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadBannerAd();
   }
 
   @override
@@ -44,38 +77,42 @@ class _SearchPageState extends State<SearchPage> {
         backgroundColor: Colors.white,
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              onChanged: (value) {
-                setState(() {
-                  _searchQuery = value;
-                });
-              },
-              textAlign: TextAlign.center,
-              controller: searchController,
-              maxLines: 1,
-              maxLength: 50,
-              style: GoogleFonts.spaceGrotesk(),
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.search),
-                  onPressed: _search,
-                ),
-                labelText: 'aramaGir'.tr,
-                focusColor: Colors.black,
-                labelStyle: GoogleFonts.indieFlower(
-                  color: const Color.fromARGB(255, 130, 126, 126),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        // Bu kısmı ekledik
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                onChanged: (value) {
+                  setState(() {
+                    _searchQuery = value;
+                  });
+                },
+                textAlign: TextAlign.center,
+                controller: searchController,
+                maxLines: 1,
+                maxLength: 50,
+                style: GoogleFonts.spaceGrotesk(),
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.search),
+                    onPressed: _search,
+                  ),
+                  labelText: 'aramaGir'.tr,
+                  focusColor: Colors.black,
+                  labelStyle: GoogleFonts.indieFlower(
+                    color: const Color.fromARGB(255, 130, 126, 126),
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: ListView.builder(
+            ListView.builder(
               physics: const BouncingScrollPhysics(),
+              shrinkWrap:
+                  true, // ListView'ın boyutunu içindeki elemanlara göre ayarlamak için
               itemCount: _searchResults.length,
               itemBuilder: (context, index) {
                 DocumentSnapshot document = _searchResults[index];
@@ -113,13 +150,15 @@ class _SearchPageState extends State<SearchPage> {
                               onPressed: () async {
                                 String googleMapsUrl =
                                     'https://www.google.com/maps/search/?api=1&query=${document['kayitliKonum']}';
+                                // ignore: deprecated_member_use
                                 if (await canLaunch(googleMapsUrl)) {
+                                  // ignore: deprecated_member_use
                                   await launch(googleMapsUrl);
                                 } else {
                                   throw 'Google Haritalar açılamadı.';
                                 }
                               },
-                              icon: Icon(
+                              icon: const Icon(
                                 Icons.arrow_outward_outlined,
                               ),
                             ),
@@ -131,8 +170,13 @@ class _SearchPageState extends State<SearchPage> {
                 );
               },
             ),
-          ),
-        ],
+            if (_isBannerAdLoaded)
+              SizedBox(
+                height: 50, // Reklamın yüksekliği
+                child: AdWidget(ad: _bannerAd!),
+              ),
+          ],
+        ),
       ),
     );
   }
